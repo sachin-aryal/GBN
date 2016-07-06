4c06fffe54b0b1c0737b15ae3c077f74afb8612a
koolzephyr <sameer.koirala@deerwalk.edu.np>
2016-07-06 23:20:34 +0545

    Parser completed

---
+++ src/main/java/Service/Parser.java
@@ -0,0 +1,71 @@
+package Service;
+
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+/**
+ * Created by anons on 7/6/16.
+ */
+
+
+public class Parser {
+    public Hashtable getDataFromFile(String file){
+        Hashtable dictionary = new Hashtable();
+        Path inPath = Paths.get(file);
+        Path outPath = Paths.get("/home/anons/Documents/project/sortedposnegword.txt");
+        Map<String,Double> itemMap = new TreeMap();
+        try (BufferedReader reader = Files.newBufferedReader(inPath)) {
+            reader.lines()
+                    .distinct()
+                    .map(line->Arrays.asList(line.split(",")))
+                    .collect(Collectors.toList())
+                    .forEach(item->{
+                        String[] eachItem =  item.toString().split(",");
+
+                        String key = eachItem[4].split("#")[0].trim();
+                        Double sentimentValue = (Double.parseDouble(eachItem[2].trim())-Double.parseDouble(eachItem[3].trim()));
+
+                        if (itemMap.containsKey(key)){
+                            Double oldSentimentValue = itemMap.get(key);
+                            if (Math.abs(oldSentimentValue)<Math.abs(sentimentValue)){
+                                itemMap.put(key, sentimentValue);
+                            }
+                        }else {
+                            itemMap.put(key,sentimentValue);
+                        }
+                    });
+
+            /*Set set = itemMap.entrySet();
+            Iterator it = set.iterator();
+
+            while(it.hasNext()) {
+                Map.Entry me = (Map.Entry)it.next();
+                System.out.println(me.getKey() + ","+me.getValue());
+            }*/
+
+            BufferedWriter writer = Files.newBufferedWriter(outPath);
+            itemMap.forEach((key,value)->{
+                try {
+                    writer.append(key).append(',').append(value.toString()).append('\n');
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return dictionary;
+    }
+
+    public static void main(String[] args) {
+        Parser pa = new Parser();
+        pa.getDataFromFile("/home/anons/Documents/project/sentiword.txt");
+    }
+}
