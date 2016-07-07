645ca30eceda1f5bbd75294bc47bbfc59517ef1a
sachin-aryal <saaryal51@gmail.com>
2016-07-07 09:33:45 +0545

    Target language Set to Java 8

--- src/main/java/Service/Crawler.java
+++ src/main/java/Service/Crawler.java
@@ -4,7 +4,6 @@ import Model.News;
 import io.vertx.core.json.JsonObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
